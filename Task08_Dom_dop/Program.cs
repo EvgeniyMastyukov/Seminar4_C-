99052e66502da25f37a47d792a90967f59801ba5
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-06-10 22:51:46 +0300

    Add Task 08

---
+++ Task08_Dom_dop/Program.cs
@@ -0,0 +1,42 @@
+﻿//Доп. задача: Напишите функцию, которая принимает слово и возвращает true, если в этом 
+//слове есть две одинаковые, стоящие рядом буквы. "ссора" -> true "клюющий" -> true
+// "кодер" -> false "цыпленок"-> false
+
+Console.WriteLine("Введите любое слово: ");
+string word = Console.ReadLine();
+int size = word.Length;
+for (int i = 0; i < size - 1; i++)
+{
+    char letter = word[i];
+
+    for (int j = i + 1; j < size; j++)
+    {
+        if (word[j] == letter)
+        {
+             Console.WriteLine($"Слово {word} - true");
+        }
+        else
+        {
+            Console.WriteLine($"Слово {word} - false");
+        }
+        break;
+    }
+
+}
+
+
+// Console.WriteLine("Введите любое слово: ");
+// string word = Console.ReadLine();
+// int size = word.Length;
+// for (int i = 0; i < size-1; i++)
+// {
+//     char letter = word[i];
+//     for (int j = i + 1; j < size; j++)
+//     {
+//         bool sovpad = word[j] == letter;       
+//         Console.WriteLine(sovpad? "True":"False" );
+//         break;
+
+//     }
+
+// }
